094a3fe82604c16ef6adaf4a1e728a0050bebe9a
amanKumar72 <amankumar721088@gmail.com>
2024-10-07 13:05:27 +0530

    day 2

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
 
-public class ScannerDemo{
+public class ScannerDemo {
+  static int countWords(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ' ' || s.charAt(i) == '\n' || s.charAt(i) == '\t') {
+        count++;
+      }
+    }
+    if (!(s.charAt(s.length() - 1) == '\n' || s.charAt(s.length() - 1) == '\t' || s.charAt(s.length() - 1) == ' ')) {
+      count++;
+    }
+
+    // int count = 0;
+    // Scanner sc=new Scanner(s);
+    // while(sc.hasNext()){
+    // sc.next();
+    // count++;
+
+    // String[] str=s.trim().split(" ");
+    // return str.length;
+
+    return count;
+  }
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    
+    System.out.println(countWords("h\nhsaj"));
   }
 }
